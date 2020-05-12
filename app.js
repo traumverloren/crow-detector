c1c14ecce3b41e19176b3248ae3642997221da6d
Stephanie Nemeth <traumverloren@gmail.com>
2020-05-12 19:54:37 +0200

    try motion detection

--- app.js
+++ app.js
@@ -3,22 +3,49 @@ const predict = require('./predict');
 
 const DEFAULT_MODEL_LOCATION = `file:///${__dirname}/model/model.json`;
 
-let model;
+// let model;
 
-(async function main() {
-  try {
-    model = await tf.loadLayersModel(DEFAULT_MODEL_LOCATION);
-    model.summary();
+// (async function main() {
+//   try {
+//     model = await tf.loadLayersModel(DEFAULT_MODEL_LOCATION);
+//     model.summary();
 
-    // Gets the ranked list
-    const results = await predict(model);
+//     // Gets the ranked list
+//     const results = await predict(model);
 
-    // Get the top result's name
-    const topResult = results[0].className;
-    console.log('**************************');
-    console.log('Prediction:', topResult);
-    console.log('**************************');
-  } catch (e) {
-    console.error(e);
-  }
-})();
+//     // Get the top result's name
+//     const topResult = results[0].className;
+//     console.log('**************************');
+//     console.log('Prediction:', topResult);
+//     console.log('**************************');
+//   } catch (e) {
+//     console.error(e);
+//   }
+// })();
+
+// Johnny-Five for RPi
+const raspi = require('raspi-io');
+const five = require('johnny-five');
+const board = new five.Board({ io: new raspi() });
+
+board.on('ready', () => {
+  console.log('board is ready');
+
+  // Create a new `motion` hardware instance.
+  const motion = new five.Motion('P1-7'); //a PIR is wired on pin 7 (GPIO 4)
+
+  // 'calibrated' occurs once at the beginning of a session
+  motion.on('calibrated', () => {
+    console.log('MOTION CALIBRATED');
+  });
+
+  // Motion detected
+  motion.on('motionstart', () => {
+    console.log('MOTION STARTED');
+  });
+
+  // 'motionend' events
+  motion.on('motionend', () => {
+    console.log('MOTION ENDED');
+  });
+});
