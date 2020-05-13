0124cc75ad85f16f89448805335fda29f71b21e5
Stephanie Nemeth <traumverloren@gmail.com>
2020-05-13 17:24:20 +0200

    add rpi-gpio

--- app.js
+++ app.js
@@ -24,30 +24,53 @@ const DEFAULT_MODEL_LOCATION = `file:///${__dirname}/model/model.json`;
 // })();
 
 // Johnny-Five for RPi
-const Raspi = require('raspi-io').RaspiIO;
-const five = require('johnny-five');
-const board = new five.Board({
-  io: new Raspi(),
-});
+// const Raspi = require('raspi-io').RaspiIO;
+// const five = require('johnny-five');
+// const board = new five.Board({
+//   io: new Raspi(),
+// });
 
-board.on('ready', () => {
-  console.log('board is ready');
+// board.on('ready', () => {
+//   console.log('board is ready');
 
-  // Create a new `motion` hardware instance.
-  const motion = new five.Motion('P1-7'); //a PIR is wired on pin 7 (GPIO 4)
+//   // Create a new `motion` hardware instance.
+//   const motion = new five.Motion('P1-7'); //a PIR is wired on pin 7 (GPIO 4)
 
-  // 'calibrated' occurs once at the beginning of a session
-  motion.on('calibrated', () => {
-    console.log('MOTION CALIBRATED');
+//   // 'calibrated' occurs once at the beginning of a session
+//   motion.on('calibrated', () => {
+//     console.log('MOTION CALIBRATED');
+//   });
+
+//   // Motion detected
+//   motion.on('motionstart', () => {
+//     console.log('MOTION STARTED');
+//   });
+
+//   // 'motionend' events
+//   motion.on('motionend', () => {
+//     console.log('MOTION ENDED');
+//   });
+// });
+
+const gpio = require('rpi-gpio');
+const pir = {
+  pin: 7,
+  loopTime: 1500, // check the sensor this often
+  tripped: false,
+  value: undefined,
+};
+const readInterval = () => {
+  gpio.read(pir.pin, (error, value) => {
+    // we only want to move on if something changed
+    if (value === pir.tripped) return (pir.tripped = value);
+    if (pir.tripped) console.log('tripped!');
+    else console.log("it's quiet... a little TOO quiet...");
   });
+};
+const onSetup = (error) => {
+  if (error) console.error(error);
+  return setInterval(readInterval, pir.loopTime);
+};
 
-  // Motion detected
-  motion.on('motionstart', () => {
-    console.log('MOTION STARTED');
-  });
-
-  // 'motionend' events
-  motion.on('motionend', () => {
-    console.log('MOTION ENDED');
-  });
-});
+gpio.setMode(gpio.MODE_RPI);
+gpio.setup(pir.pin, gpio.DIR_IN, onSetup);
