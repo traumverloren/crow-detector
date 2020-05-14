b10a334a4e6e40b0d3605a42cd9b4b6e318afa11
Stephanie Nemeth <traumverloren@gmail.com>
2020-05-14 23:05:19 +0200

    add some more notes for myself

--- camera.js
+++ camera.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const { spawn, fork } = require('child_process');
 let count = 0;
 
+const CROW = 'crow';
+
 // Take a photo with the camera module using Raspistill on the command line with spawn
 function takePhoto() {
   let filename = `${__dirname}/photos/image_${count}.jpg`;
@@ -13,13 +15,21 @@ function takePhoto() {
 
     if (count % 5 === 0) {
       console.log(filename);
-      // Child process: read the first image and detect crow with trained model
+      // Use child_process fork():
+      // read the first image and use trained model to detect if there's a crow
       let checkPhoto = fork('./detect.js');
       checkPhoto.send(filename);
 
-      // the child process is completed
+      // Get result from model
       checkPhoto.on('message', data => {
         console.log(data);
+
+        if (data === CROW) {
+          // upload to twitter
+        }
+
+        // TODO: Delete photos to clean up space
+        // deletePhoto(filename);
       });
     }
 
